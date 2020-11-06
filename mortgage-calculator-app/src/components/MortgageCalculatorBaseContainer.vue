9f15043943ab96bf69b275810c70a81471be3e55
janocano <j_janiszewski@live.com>
2020-11-06 12:53:28 -0500

    make container take prop for title and slot for rest of content

--- mortgage-calculator-app/src/components/MortgageCalculatorBaseContainer.vue
+++ mortgage-calculator-app/src/components/MortgageCalculatorBaseContainer.vue
@@ -1,12 +1,20 @@
 <template>
     <div class="container">
+        <div class="container__title">
+            {{ title }}
+        </div>
+        <slot></slot>
     </div>
 </template>
 <script>
 export default {
-    
-}
+    props: {
+        title: {
+            type: String,
+            default: ""
+        }
+    }
+};
 </script>
 <style scoped>
-
 </style>
