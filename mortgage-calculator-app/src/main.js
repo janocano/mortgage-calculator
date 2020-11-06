df32c5bcb4cd95a7d1f4a7051dc9ccb13a181f4b
janocano <j_janiszewski@live.com>
2020-11-06 12:27:41 -0500

    rename main app page to MortgageCalculatorPage

--- mortgage-calculator-app/src/main.js
+++ mortgage-calculator-app/src/main.js
@@ -1,11 +1,11 @@
 import Vue from 'vue';
-import App from './App.vue';
+import MortgageCalculatorPage from './MortgageCalculatorPage.vue';
 
 import store from "./store/main.js";
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
+  render: h => h(MortgageCalculatorPage),
   store
 }).$mount('#app')
