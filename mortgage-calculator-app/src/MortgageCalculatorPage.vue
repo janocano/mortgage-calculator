4d15b3552810d9d0562801f6bcbde95c3747e5ec
janocano <j_janiszewski@live.com>
2020-11-09 21:30:05 -0500

    add MortgageCalculatorPaymentPlanBox into the main page

--- mortgage-calculator-app/src/MortgageCalculatorPage.vue
+++ mortgage-calculator-app/src/MortgageCalculatorPage.vue
@@ -1,13 +1,16 @@
 <template>
   <div id="app" class="mortgageCalculatorPage">
     <div class="mortgageCalculatorPage__title">Mortgage Calculator</div>
+    <MortgageCalculatorPaymentPlanBox />
  </div>
 </template>
 
 <script>
+import MortgageCalculatorPaymentPlanBox from "./components/MortgageCalculatorPaymentPlanBox.vue";
 export default {
   name: 'MortgageCalculatorPage',
   components: {
+    MortgageCalculatorPaymentPlanBox
   },
 }
 </script>
