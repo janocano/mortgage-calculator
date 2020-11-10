69452f461b6f60244a1dac1ee2dd16d634536e6c
janocano <j_janiszewski@live.com>
2020-11-09 21:29:12 -0500

    add stuff to slot with styles in paymentPlanBox

--- mortgage-calculator-app/src/components/MortgageCalculatorPaymentPlanBox.vue
+++ mortgage-calculator-app/src/components/MortgageCalculatorPaymentPlanBox.vue
@@ -1,5 +1,7 @@
 <template>
     <MortgageCaculatorBaseContainer title="Payment Plan">
+        <div class="paymentPlanBox">
+        </div>
     </MortgageCaculatorBaseContainer>
 </template>
 <script>
@@ -11,5 +13,11 @@ export default {
 }
 </script>
 <style scoped>
+.paymentPlanBox {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    padding: 20px;
+}
 
 </style>
