824c243c021ab78b3ea538e377f9ae130f30bb65
janocano <j_janiszewski@live.com>
2020-11-09 21:00:09 -0500

    add button logic, template and styles

--- mortgage-calculator-app/src/components/BaseButton.vue
+++ mortgage-calculator-app/src/components/BaseButton.vue
@@ -1,11 +1,39 @@
 <template>
-    
+    <button class="button" :disabled="isDisabled">
+		<div><slot name="text"></slot></div>
+    </button>
 </template>
 <script>
 export default {
-    
+    props: {
+        isDisabled: {
+            type: Boolean,
+            default: false
+        }
+    },
 }
 </script>
 <style scoped>
-
+.button {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+    height: 25px;
+    width: fit-content;
+	border: 0px;
+	border-radius: 5px;
+    background-color: #005DAA;
+	font-size: 14px;
+	font-weight: bold;
+	color: #FFD200;
+	padding: 10px 20px;
+	outline: none;
+	overflow: hidden;
+	white-space: nowrap;
+	text-overflow: ellipsis;
+	cursor: pointer;
+}
+.button:hover {
+    background-color: #004f8f;
+}
 </style>
