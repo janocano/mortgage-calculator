a04e921d7593d9475a6ea2f8be2905e8906a3fea
janocano <j_janiszewski@live.com>
2020-11-09 21:32:23 -0500

    import baseformfield into mortgage calculator

--- mortgage-calculator-app/src/components/MortgageCalculatorPaymentPlanBox.vue
+++ mortgage-calculator-app/src/components/MortgageCalculatorPaymentPlanBox.vue
@@ -5,9 +5,11 @@
     </MortgageCaculatorBaseContainer>
 </template>
 <script>
+import BaseFormfield from "./BaseFormfield.vue";
 import MortgageCaculatorBaseContainer from "./MortgageCalculatorBaseContainer.vue";
 export default {
     components: {
+        BaseFormfield,
         MortgageCaculatorBaseContainer
     }
 }
