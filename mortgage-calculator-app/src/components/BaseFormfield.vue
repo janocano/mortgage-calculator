05c6229c4daa47115c38b4557e06ea21b4456a08
janocano <j_janiszewski@live.com>
2020-11-09 19:29:43 -0500

    add props to baseFormfield component and intialize template

--- mortgage-calculator-app/src/components/BaseFormfield.vue
+++ mortgage-calculator-app/src/components/BaseFormfield.vue
@@ -1,9 +1,40 @@
 <template>
-    
+    <div class="formfield">
+        <input 
+            :id="id"
+            class="formfield__input" 
+            :class="{ 'formfield__input--disabled': isDisabled }"
+            :type="type"
+            :disabled="isDisabled"
+            :value="value"
+            :placeholder="placeholder"
+            @input="handleInput"
+        />
+    </div>
 </template>
 <script>
 export default {
-    
+    props: {
+        placeholder: {
+            type: String,
+            default: ""
+        },
+        id: {
+            type: String,
+            default: ""
+        },
+        type: {
+            type: String,
+            default: "text"
+        },
+        value: {
+            type: [String, Number]
+        },
+        isDisabled: {
+            type: Boolean,
+            default: false
+        },
+    },
 }
 </script>
 <style scoped>
