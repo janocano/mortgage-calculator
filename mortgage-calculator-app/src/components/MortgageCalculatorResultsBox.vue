f315a007b3b410d038c754e8652be3c11ade8fa0
janocano <j_janiszewski@live.com>
2020-11-11 22:49:30 -0500

    initialize results box component

---
+++ mortgage-calculator-app/src/components/MortgageCalculatorResultsBox.vue
@@ -0,0 +1,12 @@
+<template>
+    <MortgageCalculatorBaseContainer title="Results">
+    </MortgageCalculatorBaseContainer>
+</template>
+<script>
+import MortgageCalculatorBaseContainer from "./MortgageCalculatorBaseContainer.vue";
+export default {
+    components: {
+        MortgageCalculatorBaseContainer
+    },
+}
+</script>
