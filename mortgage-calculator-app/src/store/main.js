4f9cc7503825b32a13bb0d2a48aae36521f38341
janocano <j_janiszewski@live.com>
2020-11-11 20:36:39 -0500

    add payment plan and mutation to store

--- mortgage-calculator-app/src/store/main.js
+++ mortgage-calculator-app/src/store/main.js
@@ -10,6 +10,7 @@ export default new Vuex.Store({
         amortizationPeriodYears: "25 years",
         interestRate: "5.00",
         mortgagePayment: "100000",
+        paymentPlan: ""
     },
     mutations: {
         /**
@@ -17,22 +18,30 @@ export default new Vuex.Store({
          * @param {String} payload.amortizationPeriodYears
          */
         ["SET_AMORTIZATION_PERIOD_YEARS"](state, payload) {
-            state.amortizationPeriodYears = payload.amortizationPeriodYears
+            state.amortizationPeriodYears = payload.amortizationPeriodYears;
         },
         /**
          * @param {Object} payload 
          * @param {String} payload.interestRate
          */
         ["SET_INTEREST_RATE"](state, payload) {
-            state.interestRate = payload.interestRate
+            state.interestRate = payload.interestRate;
         },
         /**
          * @param {Object} payload 
          * @param {String} payload.mortgagePayment
          */
         ["SET_MORTGAGE_PAYMENT"](state, payload) {
-            state.mortgagePayment = payload.mortgagePayment
+            state.mortgagePayment = payload.mortgagePayment;
         },
+        /**
+         * @param {Object} payload 
+         * @param {String} payload.mortgagePayment
+         */
+        ["SET_PAYMENT_PLAN"](state, payload) {
+            state.paymentPlan = payload.paymentPlan;
+        },
+
     },
     actions: {
         /**
