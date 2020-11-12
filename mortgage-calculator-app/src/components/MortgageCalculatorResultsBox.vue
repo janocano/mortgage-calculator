65dafed729466e039780d5e558c0acf472f19fa5
janocano <j_janiszewski@live.com>
2020-11-11 23:00:03 -0500

    add empty section

--- mortgage-calculator-app/src/components/MortgageCalculatorResultsBox.vue
+++ mortgage-calculator-app/src/components/MortgageCalculatorResultsBox.vue
@@ -5,6 +5,7 @@
             <span class="resultsBox__price--smallerFont">per month</span>
         </div>
         <div class="resultsBox__empty">
+            Nothing to display
         </div>
     </MortgageCalculatorBaseContainer>
 </template>
@@ -23,6 +24,7 @@ export default {
 <style scoped>
 .resultsBox__price {
     margin: 8px;
+    padding: 15px;
     background: rgba(255, 208, 0, 0.5);
     font-size: 32px;
     font-weight: bold;
@@ -30,5 +32,16 @@ export default {
 }
 .resultsBox__price--smallerFont {
     font-size: 24px;
+}
+.resultsBox__empty {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 8px;
+    padding: 15px;
+    background: rgba(0, 93, 170, 0.5);
+    font-size: 32px;
+    font-weight: bold;
+    color: #005DAA;
 }
 </style>
