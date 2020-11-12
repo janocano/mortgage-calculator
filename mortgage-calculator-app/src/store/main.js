fb0ba8372624eaf0a3f45c03492a04dd08857d6a
janocano <j_janiszewski@live.com>
2020-11-11 19:45:24 -0500

    add action for populating the years dropdown

--- mortgage-calculator-app/src/store/main.js
+++ mortgage-calculator-app/src/store/main.js
@@ -3,9 +3,11 @@ import Vue from 'vue';
 
 Vue.use(Vuex);
 
+const MAX_AMORTIZATION_YEARS = 30;
+
 export default new Vuex.Store({
     state: {
-        amortizationPeriodYears: "25 Years",
+        amortizationPeriodYears: "25 years",
         interestRate: "5.00",
         mortgagePayment: "100000",
     },
@@ -31,5 +33,21 @@ export default new Vuex.Store({
         ["SET_MORTGAGE_PAYMENT"](state, payload) {
             state.mortgagePayment = payload.mortgagePayment
         },
+    },
+    actions: {
+        /**
+         * @returns {Promise}
+         */
+        getAmortizationYearOptions({}) {
+            try {
+                let years = [];
+                for (let i = 1; i <= MAX_AMORTIZATION_YEARS; i++) {
+                    years.push(`${i} years`)
+                }
+                return Promise.resolve(years);
+            } catch (error) {
+                return Promise.reject(error);
+            }
+        }
     }
 });
