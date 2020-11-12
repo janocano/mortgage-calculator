5e5f62b14a0cb86e78ca50ed12f31fdac7b838aa
janocano <j_janiszewski@live.com>
2020-11-11 22:58:25 -0500

    add mortgage payment permonth section and some styling

--- mortgage-calculator-app/src/components/MortgageCalculatorResultsBox.vue
+++ mortgage-calculator-app/src/components/MortgageCalculatorResultsBox.vue
@@ -1,16 +1,34 @@
 <template>
     <MortgageCalculatorBaseContainer title="Results">
         <div class="resultsBox__price">
+            {{ mortgageResultPerMonth }}
+            <span class="resultsBox__price--smallerFont">per month</span>
         </div>
         <div class="resultsBox__empty">
         </div>
     </MortgageCalculatorBaseContainer>
 </template>
 <script>
+import { mapState } from 'vuex';
 import MortgageCalculatorBaseContainer from "./MortgageCalculatorBaseContainer.vue";
 export default {
     components: {
         MortgageCalculatorBaseContainer
     },
+    computed: {
+        ...mapState(["mortgageResultPerMonth"]),
+    }
 }
 </script>
+<style scoped>
+.resultsBox__price {
+    margin: 8px;
+    background: rgba(255, 208, 0, 0.5);
+    font-size: 32px;
+    font-weight: bold;
+    color: #005DAA;
+}
+.resultsBox__price--smallerFont {
+    font-size: 24px;
+}
+</style>
