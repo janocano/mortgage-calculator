e8fb3d6dad88356d6224ac2d585715ad37f232b3
janocano <j_janiszewski@live.com>
2020-11-12 20:12:57 -0500

    add data property boolean to disable button while calculating the results

--- mortgage-calculator-app/src/components/MortgageCalculatorSubmitButton.vue
+++ mortgage-calculator-app/src/components/MortgageCalculatorSubmitButton.vue
@@ -1,19 +1,28 @@
 <template>
-    <BaseButton @click.native="calculateMortgage()">
+    <BaseButton :isDisabled="isCalculatingResults"  @click.native="calculateMortgage()">
         <template slot="text">
             Calculate
         </template>
     </BaseButton>
 </template>
 <script>
+import { mapState } from "vuex";
 import BaseButton from "./BaseButton.vue";
 export default {
     components: {
         BaseButton
     },
+    data() {
+        return {
+            isCalculatingResults: false
+        };
+    },
+    computed: {
+        ...mapState([]),
+    },
     methods: {
         calculateMortgage(){
-            
+
         }
     }
 }
