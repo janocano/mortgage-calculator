097bb0b59ee15d092588fb105d6fee9a1680c1d3
janocano <j_janiszewski@live.com>
2020-11-12 20:33:36 -0500

    add three computed properties to submit button

--- mortgage-calculator-app/src/components/MortgageCalculatorSubmitButton.vue
+++ mortgage-calculator-app/src/components/MortgageCalculatorSubmitButton.vue
@@ -8,6 +8,8 @@
 <script>
 import { mapState } from "vuex";
 import BaseButton from "./BaseButton.vue";
+
+const MONTHS_IN_A_YEAR = 12;
 export default {
     components: {
         BaseButton
@@ -19,7 +21,26 @@ export default {
     },
     computed: {
         ...mapState(["amortizationPeriodYears", "interestRate", "mortgagePayment", "mortgageTerm", "paymentFrequency", "prepaymentAmount", "prepaymentFrequency", "prepaymentYear"]),
-        
+        /**
+         * @returns {Number}
+         */
+        convertedInterestRate() {
+            let interestRateNoCommas = this.interestRate.replace(",", "");
+            return parseFloar(interestRateNoCommas);
+        },
+        /**
+         * @returns {Number}
+         */
+        convertedMortgagePayment() {
+            let mortgagePaymentNoCommas = this.mortgagePayment.replace(",", "");
+            return parseFloar(mortgagePaymentNoCommas);
+        },
+        /**
+         * @returns {Number}
+         */
+        monthlyInterestRate() {
+            return this.convertedInterestRate/MONTHS_IN_A_YEAR;
+        },
     },
     methods: {
         calculateMortgage(){
