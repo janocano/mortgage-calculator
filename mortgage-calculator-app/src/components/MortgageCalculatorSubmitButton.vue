55fc2d96bb93045d18e099a3757e94fa12d9303b
janocano <j_janiszewski@live.com>
2020-11-12 20:06:59 -0500

    import BaseButton into submitbutton component and add it to template

--- mortgage-calculator-app/src/components/MortgageCalculatorSubmitButton.vue
+++ mortgage-calculator-app/src/components/MortgageCalculatorSubmitButton.vue
@@ -1,9 +1,16 @@
 <template>
-    
+    <BaseButton>
+        <template slot="text">
+            Calculate
+        </template>
+    </BaseButton>
 </template>
 <script>
+import BaseButton from "./BaseButton.vue";
 export default {
-    
+    components: {
+        BaseButton
+    },
 }
 </script>
 <style scoped>
