f06ec48cb7860e1d8736dd61479cf9305513c76b
janocano <j_janiszewski@live.com>
2020-11-16 20:03:34 -0500

    initialize test file MortgageCalculatorResultsBox

---
+++ mortgage-calculator-app/tests/unit/MortgageCalculatorResultsBox.spec.js
@@ -0,0 +1,11 @@
+import { createLocalVue, shallowMount } from '@vue/test-utils';
+import Vuex from "vuex";
+
+import MortgageCalculatorResultsBox from "../../src/components/MortgageCalculatorResultsBox.vue";
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+describe("MortgageCalculatorResultsBox.vue", () => {
+
+});
