f8c86e883fea5d567194f4c8f813d2168a747e73
janocano <j_janiszewski@live.com>
2020-11-16 19:05:43 -0500

    wrap current tests in a description to group them together

--- mortgage-calculator-app/tests/unit/MortgageCalculatorSubmitButton.spec.js
+++ mortgage-calculator-app/tests/unit/MortgageCalculatorSubmitButton.spec.js
@@ -24,32 +24,35 @@ describe("MortgageCalculatorSubmitButton.vue", () => {
         });
         wrapper = shallowMount(MortgageCalculatorSubmitButton, { localVue, store, stubs });
     });
-    it("does not disable the button from click if the button is not calculating the results and all the required fields are filled in.", async () => {
-        await wrapper.setData({ isCalculatingResults: false });
-        expect(wrapper.find("BaseButton-stub").attributes()).not.toContain("isdisabled");
+    describe("Disabling the button", () => {
+        it("does not disable the button from click if the button is not calculating the results and all the required fields are filled in.", async () => {
+            await wrapper.setData({ isCalculatingResults: false });
+            expect(wrapper.find("BaseButton-stub").attributes()).not.toContain("isdisabled");
+        });
+        it("disables the button from click if the button is calculating the results.", async () => {
+            await wrapper.setData({ isCalculatingResults: true });
+            expect(wrapper.find("BaseButton-stub").attributes("isdisabled")).toBe("true");
+        });
+        it("disables the button from click if the amortization period is not yet chosen.", async () => {
+            state.amortizationPeriodYears = -1;
+            await wrapper.vm.$nextTick();
+            expect(wrapper.find("BaseButton-stub").attributes("isdisabled")).toBe("true");
+        });
+        it("disables the button from click if the interest rate is empty.", async () => {
+            state.interestRate = "";
+            await wrapper.vm.$nextTick();
+            expect(wrapper.find("BaseButton-stub").attributes("isdisabled")).toBe("true");
+        });
+        it("disables the button from click if the mortgage payment is empty.", async () => {
+            state.mortgagePayment = "";
+            await wrapper.vm.$nextTick();
+            expect(wrapper.find("BaseButton-stub").attributes("isdisabled")).toBe("true");
+        });
+        it("disables the button from click if the payment frequency is not yet chosen.", async () => {
+            state.paymentFrequency = -1;
+            await wrapper.vm.$nextTick();
+            expect(wrapper.find("BaseButton-stub").attributes("isdisabled")).toBe("true");
+        });
     });
-    it("disables the button from click if the button is calculating the results.", async () => {
-        await wrapper.setData({ isCalculatingResults: true });
-        expect(wrapper.find("BaseButton-stub").attributes("isdisabled")).toBe("true");
-    });
-    it("disables the button from click if the amortization period is not yet chosen.", async () => {
-        state.amortizationPeriodYears = -1;
-        await wrapper.vm.$nextTick();
-        expect(wrapper.find("BaseButton-stub").attributes("isdisabled")).toBe("true");
-    });
-    it("disables the button from click if the interest rate is empty.", async () => {
-        state.interestRate = "";
-        await wrapper.vm.$nextTick();
-        expect(wrapper.find("BaseButton-stub").attributes("isdisabled")).toBe("true");
-    });
-    it("disables the button from click if the mortgage payment is empty.", async () => {
-        state.mortgagePayment = "";
-        await wrapper.vm.$nextTick();
-        expect(wrapper.find("BaseButton-stub").attributes("isdisabled")).toBe("true");
-    });
-    it("disables the button from click if the payment frequency is not yet chosen.", async () => {
-        state.paymentFrequency = -1;
-        await wrapper.vm.$nextTick();
-        expect(wrapper.find("BaseButton-stub").attributes("isdisabled")).toBe("true");
-    });
+    
 });
