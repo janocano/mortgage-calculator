c7b163f52822b1fa4ed89b1e38a880f1b0376280
janocano <j_janiszewski@live.com>
2020-11-16 19:01:40 -0500

    add test for make sure the button is showing

--- mortgage-calculator-app/tests/unit/MortgageCalculatorSubmitButton.spec.js
+++ mortgage-calculator-app/tests/unit/MortgageCalculatorSubmitButton.spec.js
@@ -24,6 +24,10 @@ describe("MortgageCalculatorSubmitButton.vue", () => {
         });
         wrapper = shallowMount(MortgageCalculatorSubmitButton, { localVue, store, stubs });
     });
+    it("does not disable the button from click if the button is not calculating the results and all the required fields are filled in.", async () => {
+        await wrapper.setData({ isCalculatingResults: false });
+        expect(wrapper.find("BaseButton-stub").attributes()).not.toContain("isdisabled");
+    });
     it("disables the button from click if the button is calculating the results.", async () => {
         await wrapper.setData({ isCalculatingResults: true });
         expect(wrapper.find("BaseButton-stub").attributes("isdisabled")).toBe("true");
